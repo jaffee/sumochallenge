9c26155db7bccba8d6198f4e27f269458760580d
Matthew Jaffee <matthew.jaffee@gmail.com>
2014-12-10 13:53:09 -0500

    install app during boostrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,8 @@ apt-get install -y python-software-properties python g++ make
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
+
+
+# install app
+cd /vagrant
+npm install
